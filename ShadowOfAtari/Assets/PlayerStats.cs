f1beec8959711a5c963ec082c531ee293b02fd38
Jack Watson <uohane@gmail.com>
2018-09-28 17:04:58 +0100

    Refined Status Bars

--- ShadowOfAtari/Assets/PlayerStats.cs
+++ ShadowOfAtari/Assets/PlayerStats.cs
@@ -1,26 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlayerStats : MonoBehaviour {
+public class PlayerStats : MonoBehaviour
+{
 
     public int max_health;
     public int health;
     public int stamina;
-	// Use this for initialization
-	void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
 
+    public Slider health_bar;
+    public Slider stamina_bar;
+
+    // Use this for initialization
+    void Start()
+    {
+        health_bar.value = max_health;
+        stamina_bar.value = stamina;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (Input.GetKeyDown(KeyCode.X))
         {
-            DealDamage(10);
-            stamina -= 50;
+
+            //DealDamage(10);
+            //stamina -= 50;
+            health_bar.value = health -= 10;
+            stamina_bar.value = stamina -= 20;
         }
-        
+
     }
 
 
