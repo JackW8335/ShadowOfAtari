d638b9adf60cf8a4a5c662a267b1e0a8452c76f4
Stroudie <alexander2.stroud@live.uwe.ac.uk>
2018-10-01 15:37:00 +0100

    Added some colision and triggers

---
+++ ShadowOfAtari/Assets/CameraChange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour {
+    public Camera oldCam;
+    public Camera newCam;
+
+	void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            oldCam.enabled = false;
+            newCam.enabled = true;
+        }
+    }
+}
