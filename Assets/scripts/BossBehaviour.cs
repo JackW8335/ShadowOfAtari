68b17f144885f4d9e153ca2bb91a3a340024e0ab
Jack Watson <uohane@gmail.com>
2018-10-04 16:49:10 +0100

    Backgrounds change based on monster health.
    Stamina drain needs to be implemented so that monster difficulty affects it.

--- Assets/scripts/BossBehaviour.cs
+++ Assets/scripts/BossBehaviour.cs
@@ -1,34 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossBehaviour : MonoBehaviour {
 
     // Use this for initialization
-    private float Health = 100;
+    private float max_health = 100;
+    public float health;
+    public Slider health_bar;
+    
 
-	void Start () {
-		
-	}
+    void Start () {
+        health = max_health;
+        health_bar.value = health;
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+
+            //DealDamage(10);
+            //stamina -= 50;
+            health_bar.value = health -= 10;
+         
+        }
+    }
 
     void WeakSpotHit()// need ref to game object 
     {
         // deavtivate weak spot / turn of weak spot effect 
     }
 
-    float getHealth()
-    {
-        return Health;
-    }
-
-    void setHealth(float nwHealth)
-    {
-        Health = nwHealth;
-    }
 
 }
---
+++ Assets/scripts/DifficultyAndEnvironment.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultyAndEnvironment : MonoBehaviour
+{
+    public BossBehaviour boss;
+
+    public Camera cam;
+    public Renderer[] rend;
+    public Material[] mats;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        changeDifficulty();
+
+    }
+
+    void changeDifficulty()
+    {
+        if (boss.health <= 60)
+        {
+
+            for (int i = 0; i < rend.Length; i++)
+            {
+                rend[i].sharedMaterial = mats[0];
+            }
+        }
+        if (boss.health <= 30)
+        {
+
+            for (int i = 0; i < rend.Length; i++)
+            {
+                rend[i].sharedMaterial = mats[1];
+            }
+
+        }
+    }
+}
