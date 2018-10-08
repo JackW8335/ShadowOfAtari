5dcac8151344e4e6ca8b482f0595b2cb185c3da9
Unknown <bradley2.moss@live.uwe.ac.uk>
2018-10-08 12:53:49 +0100

    Timer

---
+++ Assets/scripts/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    public float time = 0;
+    public float bestTime = 0;
+
+    private Text score;
+
+	// Use this for initialization
+	void Start ()
+    {
+        time = Time.time;
+        score = this.GetComponent<Text>();
+
+        
+
+        score.text = time.ToString();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        time += Time.deltaTime;
+        score.text = time.ToString("F1");
+
+	}
+}
