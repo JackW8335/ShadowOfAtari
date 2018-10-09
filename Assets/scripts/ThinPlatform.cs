3ef96b92ca76ab4201c5d0e11c2a37d0500f621d
mp2-holmes <matt.p.holmes@gmail.com>
2018-10-09 10:15:25 +0100

    added thin platform prefab and script. updated player prefab moved all scripts to scripts folder changed alt button for fire one

---
+++ Assets/scripts/ThinPlatform.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ThinPlatform : MonoBehaviour {
+
+    public GameObject platform;
+    Collider2D col;
+    // Use this for initialization
+    void Start()
+    {
+        col = platform.GetComponent<BoxCollider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            col.enabled = false;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            col.enabled = true;
+        }
+    }
+}
