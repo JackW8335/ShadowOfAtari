be35dcab427edd0941084ae1b24e5d25f377f188
JamieHogg <Jamie2.Hogg@live.uwe.ac.uk>
2018-10-09 20:50:46 +0000

    Added the stab location mat and made them show when close

---
+++ Assets/showWeakSpot.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showWeakSpot : MonoBehaviour {
+
+
+    public GameObject player;
+    private float distance = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        this.gameObject.GetComponent<Renderer>().enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        checkingDistances();
+        showSpots();
+        Debug.Log(distance);
+
+    }
+
+    void checkingDistances()
+    {
+        distance = Vector2.Distance(this.transform.position, player.transform.position);
+    }
+
+    void showSpots()
+    {
+        if (distance <= 4)
+        {
+            this.gameObject.GetComponent<Renderer>().enabled = true;
+        }
+        else
+        {
+            this.gameObject.GetComponent<Renderer>().enabled = false;
+        }
+    }
+}
